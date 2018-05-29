ce58158dd036d714c2efd83c989ccb26f1629779
dathlin <hsl200909@163.com>
2018-05-29 10:10:49 +0800

    添加SignalR支持，用作实时的推送数据显示

---
+++ WebApplication1/Startup.cs
@@ -0,0 +1,18 @@
+﻿
+using Microsoft.Owin;
+using Owin;
+using WebApplication1;
+
+[assembly: OwinStartup( typeof( WebApplication1.Startup ) )]
+
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public void Configuration( IAppBuilder app )
+        {
+            app.MapSignalR( );
+        }
+    }
+}
